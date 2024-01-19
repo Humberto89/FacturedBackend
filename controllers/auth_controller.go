dbb7e83f5a7f16eb111c6dcffe6d2ed29beaff38
JAdonyMT <jmorales@redabierta.hn>
2024-01-19 14:40:03 -0600

    Agregar logs en proceso de validacion de token

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	// "fmt"
 	// "net/http"
@@ -18,6 +19,7 @@ func ValidateToken(tokenString string) error {
 
 	// Lógica para verificar el token
 	if tokenString == "" {
+		fmt.Println("token vacio")
 		return errors.New("token vacío")
 	}
 
@@ -27,38 +29,45 @@ func ValidateToken(tokenString string) error {
 	// Divide el token en partes (header, payload, firma)
 	parts := strings.Split(tokenString, ".")
 	if len(parts) != 3 {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	// Decodifica y analiza la sección de encabezado del token
 	headerData, err := base64.RawURLEncoding.DecodeString(parts[0])
 	if err != nil {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	var header map[string]interface{}
 	if err := json.Unmarshal(headerData, &header); err != nil {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	// Verificaciones específicas del encabezado
 	// Puedes agregar más validaciones según tus requisitos
 	if typ, ok := header["typ"].(string); !ok || typ != "JWT" {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	if alg, ok := header["alg"].(string); !ok || alg != "HS256" {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	// Decodifica y analiza la sección de payload del token
 	payloadData, err := base64.RawURLEncoding.DecodeString(parts[1])
 	if err != nil {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	var payload map[string]interface{}
 	if err := json.Unmarshal(payloadData, &payload); err != nil {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
@@ -66,11 +75,13 @@ func ValidateToken(tokenString string) error {
 	// Verifica la expiración del token
 	expirationTime, ok := payload["exp"].(float64)
 	if !ok {
+		fmt.Println("token invalido")
 		return tokenInvalido
 	}
 
 	expiration := time.Unix(int64(expirationTime), 0)
 	if expiration.Before(time.Now()) {
+		fmt.Println("token Expirado")
 		return errors.New("el token ha expirado")
 	}
 
